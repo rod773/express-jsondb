e1effc1f8dbdc0cbad7b64cd65e7df8ff4b1921d
rod773 <104315743+rod773@users.noreply.github.com>
2023-10-05 14:58:45 +0200

    cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import morgan from "morgan";
+import router from "./routes/index.js";
 import cors from "cors";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -11,6 +12,8 @@ const __dirname = path.dirname(__filename);
 //console.log(__dirname);
 
 const app = express();
+
+app.use(cors());
 
 // settings
 
@@ -22,14 +25,23 @@ app.set("view engine", "ejs");
 
 // middlewares
 
-app.use(cors);
-
 app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: false }));
 
 // routes
 
+app.use(router);
+
 // stattic
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// 404 handler
+
+app.use((req, res, next) => {
+  res.status(404).send("404 not found");
+});
+//
 
 export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 export default router;
