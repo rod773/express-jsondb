01584c9d77043859321978054a364ee1d4f363c3
rod773 <104315743+rod773@users.noreply.github.com>
2023-10-05 12:39:05 +0200

    added ejs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname);
 
 const app = express();
 
 // settings
 
 app.set("port", 5000);
+
+app.set("views", path.join(__dirname, "views"));
 
 // middlewares
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from "../src/app.js";
 
 async function main() {
   await app.listen(app.get("port"));
-  console.log("http://localhost:5000");
+  console.log("http://localhost:" + app.get("port"));
 }
 
 main();
