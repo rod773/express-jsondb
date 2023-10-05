c87ad28541599de9eb64a5acbf7a73bf920c62d1
rod773 <104315743+rod773@users.noreply.github.com>
2023-10-05 12:46:38 +0200

    depemdencies morgan , corrs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import morgan from "morgan";
+import cors from "cors";
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-console.log(__dirname);
+//console.log(__dirname);
 
 const app = express();
 
@@ -16,8 +18,13 @@ app.set("port", 5000);
 
 app.set("views", path.join(__dirname, "views"));
 
+app.set("view engine", "ejs");
+
 // middlewares
 
+app.use(cors);
+
+app.use(morgan("dev"));
 // routes
 
 // stattic
